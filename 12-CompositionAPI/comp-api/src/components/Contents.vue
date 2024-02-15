48c00467a475f618ce2b77ea8b33d1973d313d95
IbrahimHicyilmaz <blog.ibrahimhicyilmaz5334@gmail.com>
2024-02-15 12:19:31 +0300

    12 commit

---
+++ 12-CompositionAPI/comp-api/src/components/Contents.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-for="content in contents" :key="content.id">
+        {{ content.title }}
+    </div>
+    <div v-if="err">
+        {{ err }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import getContentsFetch from '../composables/getContentsFetch';
+import { useRoute, useRouter } from 'vue-router'
+export default {
+    setup() {
+
+        const route = useRoute()
+        const router = useRouter()
+
+        console.log('route: ',route);
+        console.log('router: ',router);
+
+        const {contents, err, getContents} = getContentsFetch();
+        getContents();
+
+        return {contents,err}
+    }
+}
+</script>
+
+<style>
+
+</style>
